ec07e101d4fdf5b75403ff9c8929149a2582e5fc
Padraic Calpin <padraic.calpin93@gmail.com>
2017-02-05 21:16:07 +0000

    Docstring for _Result, tweaks to allow for stabilizer rank in output

--- stabilizer_search/search/__init__.py
+++ stabilizer_search/search/__init__.py
@@ -6,11 +6,21 @@ The submodules each define a strategy for finding the Stabilizer Rank."""
 
 
 class _Result(object):
+    """Base class to handle pretty-printing of results from a stabilizer rank
+    search. Inhertied classes should over-write ostring with a format string 
+    which takes the arguments:
+    target_state: A string description to the target state.
+    n_qubits: The number of qubits in the search
+    chi: The number returned by the search
+    success: Did the method converge/find a smaller stabilizer rank.
+    decomposition: A string description of the decomposition found. """
     ostring = None
-    def __init__(self, target_state_string, n_qubits, success, decomposition):
+    def __init__(self, target_state_string, n_qubits, chi, 
+                 success, decomposition):
         self.target_state = target_state_string
         self.n_qubits = n_qubits
-        self.success = success
+        self.success = 'succeeded' if success else 'did not succeed'
+        self.chi = chi
         self.decomposition = decomposition
 
     def __str__(self):
@@ -20,6 +30,7 @@ class _Result(object):
         return self.ostring.format(target_state=self.target_state,
                                    n_qubits=self.n_qubits,
                                    success=self.success,
+                                   chi = self.chi
                                    decomposition=self.decomposition)
 
 
@@ -33,7 +44,8 @@ class _Search(object):
     """Callable base class for the Stabilizer Decomposition searches. This
     class is overridden by specifying a derived instance of _Result, and a
     function that takes the same arguemnts as search.dummy_func, returning if
-    the method succeeded and the resulting decomposition."""
+    the method succeeded, and the resulting stabilizer rank
+    and decomposition."""
     Result_Class = _Result
     func = dummy_func
 
@@ -46,10 +58,8 @@ class _Search(object):
         self.f_kwargs = kwargs
 
     def __call__(self):
-        success, decomposition = self.func(self.n_qubits, self.target_state,
+        success, chi, decomposition = self.func(self.n_qubits, self.target_state,
                                            *self.f_args, **self.f_kwargs)
         return self.Result_Class(self.target_state_string, self.n_qubits,
-                                 success, decomposition)
-
-
+                                 chi, success, decomposition)
         
