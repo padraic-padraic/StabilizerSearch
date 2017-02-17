640967f4140106351b23ae8f14381a9a58a6183f
Padraic Calpin <padraic.calpin93@gmail.com>
2017-02-17 15:09:46 +0000

    Add data-files for 1-3 qubits, update setup.py for first release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from Cython.Build import cythonize
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
-extensions = [
+EXTENSIONS = [
     Extension(
         "stabilizer_search.mat.haar_random",
         ["stabilizer_search/mat/haar_random.pyx"],
@@ -15,8 +15,15 @@ extensions = [
 
 setup(
     name='stabilizer_search',
-    version='0.1.0',
-    packages = find_packages(exclude=['tests', '*tests']),
+    version='1.0.0',
+    url="https://github.com/padraic-padraic/StabilizerSearch",
+    author="Padraic Calpin",
+    license="GPLv3.0",
+    keywords='quantum computing stabilizers simulation',
+    packages=find_packages(exclude=['tests', '*tests']),
+    package_data={
+        'stabilizer_search.stabilizers':['/data/*.pkl']
+    },
     install_requires=['cython', 'numpy', 'scipy', 'six'],
-    ext_modules=cythonize(extensions)
-    )
+    ext_modules=cythonize(EXTENSIONS)
+)
